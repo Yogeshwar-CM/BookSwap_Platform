d878a2b9a4f3c4f4decbde4ab04b73c3255688a1
Yogeshwar-CM <cmyogeshwar@gmail.com>
2024-03-10 18:52:24 +0530

    Frontend of explore page

--- client/src/components/ExpSec.jsx
+++ client/src/components/ExpSec.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import "./ExpSec.css"
+import React from "react";
+import "./ExpSec.css";
+import b1 from "../assets/b1.png";
+import profile from "../assets/profile.png";
 
 const ExpSec = () => {
-    return (
-        <div className='expsec'>
-            
-        </div>
-    );
-}
+  return (
+    <div className="expsec">
+      <img src={b1} alt="" />
+      <div className="exp-dets">
+        <p>Stephen Hawking - Theory of Everything</p>
+        <p>3 Years | Chennai</p>
+        <br />
+        <br />
+        <p>Looking for any Fiction or Non Fiction books in good condition</p>
+      </div>
+      <button className="swap-btn">SWAP</button>
+      <button className="contact-btn">Contact</button>
+      <img src={profile} alt="" className="profile"/>
+    </div>
+  );
+};
 
 export default ExpSec;
